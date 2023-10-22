ea8d7d5c88f831f506dca302f2d5fbf5a222cfd0
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-22 16:12:28 +0300

    Completed task

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/ManufactureFilteringStrategy.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/ManufactureFilteringStrategy.java
@@ -1,11 +1,16 @@
 package com.mjc.stage2.impl;
 
 
+import com.mjc.stage2.FilteringStrategy;
+import com.mjc.stage2.Product;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class ManufactureFilteringStrategy {
+public class ManufactureFilteringStrategy implements FilteringStrategy {
     private String manufacture;
-
+    @Override
+    public boolean filter(Product product){
+    return manufacture.equalsIgnoreCase(product.getManufacture());
+}
     // Write your code here!
 }
--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,20 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
+import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
+
+public class RectangleObserver  implements Observer {
+    @Override
+    public void handleEvent(RectangleEvent event){
+        Rectangle rectangle = event.getSource();
+        if (rectangle!= null){
+            RectangleValues rectangleWarehouse = RectangleWarehouse.getInstance().get(rectangle.getId());
+            rectangleWarehouse.setPerimeter(rectangle.getSideA()*2+ rectangle.getSideB()*2);
+            rectangleWarehouse.setSquare(rectangle.getSideA()* rectangle.getSideB());
+        }
+    }
     // Write your code here!
 }
